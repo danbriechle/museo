30aff10783f02a7dd197ab7dbc38e205f685cee1
Daniel Briechle <danbriechle@hotmail.com>
2018-10-31 09:22:24 -0600

    created artist class and tests

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -1,26 +1,66 @@
-attributes = {
-  id: "2",
-  name: "Ansel Adams",
-  born: "1902",
-  died: "1984",
-  country: "United States"
-}
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/artist'
+require 'pry'
 
-artist = Artist.new(attributes)
-#=> #<Artist:0x00007fc2d0a6c080...>
+class ArtistTest < Minitest::Test
 
-artist.id
-#=> "2"
+  def test_it_exists
+    attributes = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist = Artist.new(attributes)
+    assert_instance_of Artist, artist
+  end
 
-artist.name
-#=> "Ansel Adams"
+  def test_it_can_return_its_attributes
+    attributes = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist = Artist.new(attributes)
+    assert_equal "2", artist.id
+    assert_equal "Ansel Adams", artist.name
+    assert_equal "1902", artist.born
+    assert_equal "1984", artist.died
+    assert_equal "United States", artist.country
+  end
+end
 
-artist.born
-#=> "1902"
 
-artist.died
-#=> "1984"
 
-artist.country
-#=> "United States"
-```
+
+
+# attributes = {
+#   id: "2",
+#   name: "Ansel Adams",
+#   born: "1902",
+#   died: "1984",
+#   country: "United States"
+# }
+#
+# artist = Artist.new(attributes)
+# #=> #<Artist:0x00007fc2d0a6c080...>
+#
+# artist.id
+# #=> "2"
+#
+# artist.name
+# #=> "Ansel Adams"
+#
+# artist.born
+# #=> "1902"
+#
+# artist.died
+# #=> "1984"
+#
+# artist.country
+# #=> "United States"
+# ```
